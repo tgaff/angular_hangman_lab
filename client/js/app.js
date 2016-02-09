9eb04db53d1b69b7a07b0bafa5db1527bf0a376f
tgaff <tgaff@alumni.nd.edu>
2016-02-08 19:55:00 -0800

    step 4 complete

--- client/js/app.js
+++ client/js/app.js
@@ -1,9 +1,19 @@
 console.log('app.js loaded!');
 
-var app = angular.module("hangmanApp", [])
+angular.module("hangmanApp", [])
   .controller('hangmanController', hangmanController);
 
 function hangmanController() {
   console.log('hangmanController online');
   this.controllerWorks = true;
+  this.game = new HangmanGame('elephant');
+  this.newGuess = '';
+  this.checkGuess = checkGuess;
+
+
+  function checkGuess() {
+    console.log('checking');
+    this.game.guess(this.newGuess);
+    this.newGuess = '';
+  }
 }
